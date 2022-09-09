5f2f1944f943723b17169849fb879156711e83d4
Worive <mine.pralito@gmail.com>
2022-09-09 15:20:48 +0200

    Set footer and header

--- templates/footer.php
+++ templates/footer.php
@@ -1,1 +1,3 @@
-<?php
+<footer class="text-center text-lg-start bg-light text-muted py-3 mt-4 border-top">
+    <span class="mb-3 mb-md-0 text-muted">Eliott Clergue - 2 DAW</span>
+</footer>
--- templates/header.php
+++ templates/header.php
@@ -1,1 +1,11 @@
-<?php
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom px-5">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <span class="fs-4">Projecte 1: Memograma</span>
+    </a>
+
+    <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Game</a></li>
+        <li class="nav-item"><a href="leaderboard.html" class="nav-link">Leaderboard</a></li>
+    </ul>
+
+</header>
