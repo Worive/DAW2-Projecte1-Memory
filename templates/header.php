9bedccfe81add44216238d5f3bbe015df483caf1
Worive <mine.pralito@gmail.com>
2022-09-17 11:37:19 +0200

    Header work with get arguments in the link

--- templates/header.php
+++ templates/header.php
@@ -1,9 +1,8 @@
 <?php
-$page = str_replace(
-        '#',
-        '',
-    basename(htmlspecialchars($_SERVER['REQUEST_URI']))
-);
+$page = explode('?', str_replace(
+    '#',
+    '',
+    basename(htmlspecialchars($_SERVER['REQUEST_URI']))))[0];
 
 
 function isActive($pageName, $page, $alsoDefault): void
@@ -24,13 +23,15 @@ function isActive($pageName, $page, $alsoDefault): void
 ?>
 
 <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom px-5">
-    <a href="/DAW2-Projecte1-Memory/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+    <a href="/DAW2-Projecte1-Memory/"
+       class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
         <span class="fs-4">Projecte 1: Memograma</span>
     </a>
 
     <ul class="nav nav-pills">
         <li class="nav-item">
-            <a href="/DAW2-Projecte1-Memory/" class="nav-link<?php isActive('memory.php', $page, true) ?>" aria-current="page">Game</a>
+            <a href="/DAW2-Projecte1-Memory/" class="nav-link<?php isActive('memory.php', $page, true) ?>"
+               aria-current="page">Game</a>
         </li>
         <li class="nav-item">
             <a href="leaderboard.php" class="nav-link<?php isActive('leaderboard.php', $page, false) ?>">Leaderboard</a>
