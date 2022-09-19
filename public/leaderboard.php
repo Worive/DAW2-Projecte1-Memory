f9f629dce51b219d47292561822de560df027c3d
Worive <mine.pralito@gmail.com>
2022-09-19 15:47:48 +0200

    Use catalan instead of english

--- public/leaderboard.php
+++ public/leaderboard.php
@@ -46,9 +46,9 @@ if (isset($_COOKIE['leaderboard'])) {
 } else {
     $content = '<div class="container">
         <div class="px-4 py-5 my-5 text-center">
-            <h1 class="display-5 fw-bold">No record found</h1>
+            <h1 class="display-5 fw-bold">Ninguna partida trobada</h1>
             <div class="col-lg-6 mx-auto">
-                <p class="lead mb-4">Be the first to play!</p>
+                <p class="lead mb-4">Siguis el primer en jugar!</p>
             </div>
         </div>
     </div>';
