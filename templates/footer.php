98000d03e9d04d03e998cfc60680ae87a8305729
Worive <dc.worive@gmail.com>
2022-09-25 09:33:11 +0200

    Fix footer so it stays to bottom

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,4 @@
-<footer class="footer text-center text-muted py-3 mt-2">
+<footer class="footer text-center text-muted py-3 mt-auto">
     <span class="mb-3 mb-md-0 text-muted">Eliott Clergue - 2 DAW</span>
 </footer>
 
